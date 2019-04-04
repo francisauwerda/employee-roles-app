d6acfb5c8ed66a32883111677975e11a15bc60ca
Francis Auwerda <fauwerda@gmail.com>
2019-04-04 07:21:06 +0200

    Customizing the cards WIP

--- src/js/components/ListItem/index.js
+++ src/js/components/ListItem/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
-import './ListItem.css';
+import styled from 'styled-components';
+import { primary, primaryLight, primaryDark } from '../../styles';
 
 const ListItem = (props) => {
-  return <li className="listItem">{props.employee.firstName}</li>;
+  return (
+    <ListItemStyled>
+      {props.employee.firstName}
+    </ListItemStyled>
+  );
 }
+
+const ListItemStyled = styled.li`
+  background-color: ${primary};
+  margin: 15px 0;
+  cursor: pointer;
+  transition: background-color .5s;
+  padding: 20px 0;
+
+  &:hover {
+    background-color: ${primaryLight};
+  }
+`;
 
 export default ListItem;
---
+++ src/js/styles.js
@@ -0,0 +1,3 @@
+export const primary = '#0d47a1';
+export const primaryLight = '#5472d3';
+export const primaryDark = '#002171';
