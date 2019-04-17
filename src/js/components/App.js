d8ce564ca1a3c07dfd9b6f1158b3dca69dd30191
Francis Auwerda <fauwerda@gmail.com>
2019-04-17 07:04:52 +0200

    Adding placeholder for form and making the screen responsive

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 import ListContainer from './ListContainer';
 import List from './List';
 
 const App = () => (
   <div>
-    <ListContainer>
-      <List />
-    </ListContainer>
+    <PageContainer>
+      <ListContainer>
+        <List />
+      </ListContainer>
+      <div>Form</div>
+    </PageContainer>
   </div>
 );
+
+const PageContainer = styled.div`
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: auto;
+
+  @media only screen and (max-width: 600px) {
+    grid-template-columns: auto;
+    grid-template-rows: auto;
+  }
+`;
 
 export default App;
