37c889e666752ca5e8d88f2bc3020489bf1f8b67
Francis Auwerda <fauwerda@gmail.com>
2019-04-22 19:45:04 +0200

    Creating the basic styles for the FormInputs. Following Material Design

--- src/js/components/common/FormInput.js
+++ src/js/components/common/FormInput.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-confusing-arrow */
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const FormInput = (props) => {
@@ -9,17 +11,70 @@ const FormInput = (props) => {
     onChange,
   } = props;
   return (
-    <div>
-      <span>{label}</span>
-      <input
+    <FormInputStyled>
+      <InputStyled
         type="text"
         value={value}
         name={name}
         onChange={onChange}
       />
-    </div>
+      <LabelStyled value={value}>{label}</LabelStyled>
+    </FormInputStyled>
   );
 };
+
+const defaultFontSize = '24px';
+const labelFontSizeNormal = defaultFontSize;
+const labelFontSizeSmall = '16px';
+const inputPaddingHorizontal = '12px';
+const inputMarginHorizontal = '8px';
+const inputBorderSize = '2px';
+const labelTop = `calc(${inputPaddingHorizontal} + ${inputMarginHorizontal} + ${inputBorderSize})`;
+
+const LabelStyled = styled.label`
+  color: gray;
+  position: absolute;
+  left: 10px;
+  pointer-events: none;
+  background-color: white;
+  padding: 0 2px;
+
+  transition: 0.2s ease all;
+
+  top: ${props => props.value ? '0' : labelTop};
+  font-size: ${props => props.value ? labelFontSizeSmall : labelFontSizeNormal};
+`;
+
+const InputStyled = styled.input`
+  width: 100%;
+  box-sizing: border-box;
+  border-radius: 4px;
+  outline: none;
+
+  padding: ${inputPaddingHorizontal} 10px;
+  margin: ${inputMarginHorizontal} 0;
+  border: ${inputBorderSize} solid gray;
+  font-size: ${defaultFontSize};
+
+  &:focus {
+    border-color: blue;
+  }
+
+  &:focus ~ label {
+    top: 0;
+    font-size: ${labelFontSizeSmall};
+    color: blue;
+  }
+
+  &:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0 1000px white inset !important;
+  }
+`;
+
+const FormInputStyled = styled.div`
+  font-size: 16px;
+  position: relative;
+`;
 
 FormInput.propTypes = {
   label: PropTypes.string,
